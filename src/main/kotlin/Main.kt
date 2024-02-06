b6766511db20146b828f58cb7e7efd83a431b783
Agnes-Kamondi <kamondiagnesauma@gmail.com>
2024-02-06 23:42:14 +0300

    added kotlin files on the project

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var fullName: String = "Full Name: Agnes Auma"
+    println("Full Name: Agnes Auma")
+    var age: Byte= 21
+    println("Age :21")
+    var phoneNumber: String = "+254115052075"
+    println("Phone Number : +2541150520175")
+    var weightInKg:Double = 61.5
+    println("weight in kg :61.5")
+    var aKenyanCitizen:Boolean = true
+    println("a Kenyan Citizen")
+}
